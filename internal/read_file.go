1307ee318f3154748817553ea0ee07074f87e461
imfulee <freems5192@gmail.com>
2023-12-09 15:44:15 +0800

    feat: new way of reading files

--- internal/read_file.go
+++ internal/read_file.go
@@ -2,6 +2,7 @@ package internal
 
 import (
 	"bufio"
+	"errors"
 	"os"
 )
 
@@ -22,4 +23,31 @@ func Read(path string) ([]string, error) {
 	}
 
 	return result, nil
+}
+
+type Reader struct {
+	file *os.File
+}
+
+func (r *Reader) New(path string) error {
+	file, err := os.Open(path)
+	if err != nil {
+		return errors.Join(errors.New("unable to read file"), err)
+	}
+	defer file.Close()
+
+	r.file = file
+
+	return nil
+}
+
+func (r *Reader) Lines(lines chan string) {
+	defer close(lines)
+
+	fileScanner := bufio.NewScanner(r.file)
+	fileScanner.Split(bufio.ScanLines)
+
+	for fileScanner.Scan() {
+		lines <- fileScanner.Text()
+	}
 }
